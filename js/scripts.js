46b82bbd02f7b01bef8968f8339a843a64e17478
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-02-01 02:06:58 -0800

    Vreate the constructors for adding tasks

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,22 @@
 function todoList(){
     this.Tasks = {
 
-    }
+    };
     this.taskNo = 0;
 }
+todoList.prototype.addId = function(){
+    this.taskNo += 1;
+    return this.taskNo;
+};
+todoList.prototype.addTask = function(task){
+        task.number = this.addId();
+        this.Tasks[task.number] = task;
+    };
 
-// //Constructor For The Task
-// function Task(task , dueDate , category){
-//     this.task = task;
-//     this.dueDate = dueDate;
-//     this.category = category;
-// }
-// Task
+//Constructor For The Task
+function Task(task , dueDate , category){
+    this.task = task;
+    this.dueDate = dueDate;
+    this.category = category;
+}
+
