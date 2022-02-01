4ab09f436f8b532c634e46c5026fe6b137a53ca5
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-02-01 01:36:06 -0800

    Create the business logic for the todoList

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+// Todolist Constructor
+
+function todoList(){
+    this.Tasks = {
+
+    }
+    this.taskNo = 0;
+}
+
+// //Constructor For The Task
+// function Task(task , dueDate , category){
+//     this.task = task;
+//     this.dueDate = dueDate;
+//     this.category = category;
+// }
+// Task
